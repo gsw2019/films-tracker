f9a929b993328f84008984c5c40d5b422a034f0b
gsw2019 <wsg@arizona.edu>
2026-08-20 18:06:20 -0700

    working on Lua script and Python script brdige
    Currently the biggest issue is how to notify the Lua script of what
    changed in the .sc file. When calling script from the cell the cursor is
    on, it deletes the data there. Highly undesireable.
    
    Looking like we will have the script look over all FILM column entries
    and decide on some heuristic with how many features are blank that
    determines if we fetch data.
    
    Python script handles all fetching and returning data to Lua script

--- lua/insert_film_data.lua
+++ lua/insert_film_data.lua
@@ -4,3 +4,17 @@
 -- @author Garret Wilson
 
 
+local file = io.open("luascript_output.txt", "w")
+
+-- local col = sc.curcol()
+-- local row = sc.currow()
+--
+-- file:write(col)
+-- file:write("\n")
+-- file:write(row)
+-- file:write("\n")
+
+local str = sc.lgetstr(c, r)
+file:write(str)
+file:close()
+
